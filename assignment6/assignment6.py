f9b5e705700864aacce84e38ba128e274703164a
msdosboss <jacobt0722@gmail.com>
2024-06-02 18:09:06 -0700

    finished assignment6

--- assignment6/assignment6.py
+++ assignment6/assignment6.py
@@ -1,7 +1,8 @@
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import QSize, Qt, QTimer
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QGridLayout
 import pytest
 import time
+import random
 
 defaultLabelText = "orca"
 
@@ -11,26 +12,18 @@ class MainWindow(QMainWindow):
 		
 		self.buttonIsChecked = True
 
-		#self.label = QLabel()
-		#self.label.setText(defaultLabelText)
 		self.input = QLineEdit()
-		#self.input.textChanged.connect(self.label.setText)
+		
+		self.layout = QGridLayout()
 		
 		self.setWindowTitle(name);
 		self.button = QPushButton(buttonText)
-		#self.button.setFixedSize(QSize(500,100))
-		#self.button.setCheckable(True)
-		#self.button.clicked.connect(self.theButtonWasClicked)
-		#self.button.setChecked(self.buttonIsChecked)
 
-		#self.windowTitleChanged.connect(self.theWindowTitleChanged)		
 
-		layout = QVBoxLayout()
 
-		layout.addWidget(self.input)
-		layout.addWidget(self.button)
-		#layout.addWidget(self.label)
-
+		self.container = QWidget()
+		
+		self.labelWidgets = []
 		
 		self.setFixedSize(QSize(x, y))
 	
@@ -42,68 +35,75 @@ class MainWindow(QMainWindow):
 
 	def theWindowTitleChanged(self):
 		print("title has changed")	
-	
 
+	def addButton(self, posx, posy, lenx, leny):
+		self.layout.addWidget(self.button, posx, posy, lenx, leny)
 
-def printNumList(window, numList):
-	numList.sort()
-	currentNumber = window.label.text()	
-	if currentNumber == defaultLabelText:
-		currentNumber = None
-	
-	elif int(currentNumber) == numList[(len(numList) - 1)]:
-		currentNumber = None		
-
-	if currentNumber == None:
-		window.label.setText(str(numList[0]))
-		return
-		
-	
-	for x in range(len(numList)):
-		if int(numList[x]) == int(currentNumber):
-			window.label.setText(str(numList[x+1]))
-
-def addNumberListWidgets(window, n):
-	labelWidgets = []
-	layout = QGridLayout()
-	textField = QLineEdit()
-	button = QPushButton("Search")
-	labelWidgets.append(textField)
-	labelWidgets.append(button)
-	layout.addWidget(textField, 0, 0, 1, 7)
-	layout.addWidget(button, 0, 7, 1, 3)
-	
-	for x in range(n):
+	def addEntry(self, val, posx, posy, lenx = 1, leny = 1):
 		label = QLabel()
-		label.setText(str(x))
-		labelWidgets.append(label)
-		layout.addWidget(label, (x // 10) + 1, x % 10)
-	container = QWidget()
-	container.setLayout(layout)
-	window.setCentralWidget(container)
-	return labelWidgets
-	
-def highlightSelectedNumber(labelWidgets):
-	inputText = labelWidgets[0].text()
-	for labelWidget in labelWidgets[2:]:
-		labelWidget.setStyleSheet("")
-	
-	for labelWidget in labelWidgets[2:]:
-		labelWidget.setStyleSheet("background-color: cyan")
-		app.processEvents()		#this is considerd bad practice I should have worked my function into the processEvent loop, but instead I am just forcing it to process all events right now https://www.pythonguis.com/faq/real-time-change-of-widgets
-		time.sleep(1)
-		if labelWidget.text() == inputText:
-			break
-		labelWidget.setStyleSheet("")
+		label.setText(str(val))
+		self.labelWidgets.append(label)
+		self.layout.addWidget(self.labelWidgets[(len(self.labelWidgets) - 1)], posx, posy, lenx, leny)
+		return len(self.labelWidgets)	#return the index of the widget
 
-def testHighlightSelectedNumber():				#test the all possible inputs
-	for testText in range(100):
-		for labelWidget in labelWidgets[2:]:
-			labelWidget.setStyleSheet("background-color: cyan")
-			if labelWidget.text() == str(testText):
+	def changeEntry(self):
+		for n in range(len(self.labelWidgets)):
+			pass	
+			
+	def setGUI(self):
+		self.container.setLayout(self.layout)
+		self.setCentralWidget(self.container)
+
+	def swapEntry(self, index1, index2):
+		text1, text2 = self.labelWidgets[index1].text(), self.labelWidgets[index2].text()
+		self.labelWidgets[index1].setText(text2)
+		self.labelWidgets[index2].setText(text1)
+		
+
+def mergeSortHelperGUI(list1, list2, widgetList1Index, widgetList2Index):
+	list1Len = len(list1)
+	list2Len = len(list2)
+	if list1Len <= 1 and list2Len <= 1:
+		if int(list1[0].text()) < int(list2[0].text()):
+			return list1 + list2
+		else:
+			QTimer.singleShot(5000, lambda: swapLabels(widgetList1Index, widgetList2Index))
+			window.show()
+			return list2 + list1
+	
+	if list1Len > 1:
+		list1 = mergeSortHelperGUI(list1[0:int(list1Len / 2)], list1[int(list1Len / 2):], widgetList1Index, (widgetList1Index + int(list1Len / 2)))
+		
+	if list2Len > 1:
+		list2 = mergeSortHelperGUI(list2[0:int(list2Len / 2)], list2[int(list2Len / 2):], widgetList2Index, (widgetList2Index + int(list2Len / 2)))
+	
+	list1Index = 0
+	list2Index = 0	
+	tempList = []
+
+	for _ in range(list1Len + list2Len - 1):
+		if int(list1[list1Index].text()) < int(list2[list2Index].text()):
+			tempList.append(list1[list1Index])
+			list1Index = list1Index + 1
+			
+			if list1Index == list1Len:
+				tempList.extend(list2[list2Index:])
 				break
-			labelWidget.setStyleSheet("")
-		assert labelWidgets[testText + 2].styleSheet() == "background-color: cyan"
+		else:
+			tempList.append(list2[list2Index])
+			list2Index = list2Index + 1
+		
+			if list2Index == list2Len:
+				tempList.extend(list1[list1Index:])
+				break
+			
+	
+	return tempList
+
+
+def mergeSortGUI(listToSort):
+	listLen = len(listToSort)
+	return mergeSortHelperGUI(listToSort[0:int(listLen / 2)], listToSort[int(listLen / 2):], 0, int(listLen / 2))	
 
 
 
@@ -111,6 +111,7 @@ def mergeSortHelper(list1, list2):
 	list1Len = len(list1)
 	list2Len = len(list2)
 	if list1Len <= 1 and list2Len <= 1:
+		print(f"comparing {list1} and {list2}")
 		return sorted(list1 + list2)
 	
 	if list1Len > 1:
@@ -123,7 +124,10 @@ def mergeSortHelper(list1, list2):
 	list2Index = 0	
 	tempList = []
 
+	print(f"The lists are {list1} and {list2}")
+
 	for _ in range(list1Len + list2Len - 1):
+		print(f"comparing {list1[list1Index]} and {list2[list2Index]}")
 		if list1[list1Index] < list2[list2Index]:
 			tempList.append(list1[list1Index])
 			list1Index = list1Index + 1
@@ -147,28 +151,44 @@ def mergeSort(listToSort):
 	listLen = len(listToSort)
 	return mergeSortHelper(listToSort[0:int(listLen / 2)], listToSort[int(listLen / 2):])	
 
+def testMergeSort():
+	testList = [4, 2, 1, 6]
+	assert [1, 2, 4, 6] == mergeSort(testList)
 
-#testDictionaryEntryList(entryList)
+def swapLabels(index1, index2):
+	window.swapEntry(index1, index2)
+	print("swap")
+	window.setGUI()
 
-#printAvailableItems(entryList)
 
-#app = QApplication([])
+app = QApplication([])
 
-#window = QWidget()
-#window = QPushButton("Click Me")
-#window = MainWindow("Number searcher", "iterate")
+window = MainWindow("Number searcher", "iterate")
 
 numList = [5, 1, 3, 1, 10, 4, 15, 12, 2, 5, 21, 100, 2]
 
 print(mergeSort(numList))
 
-#window.button.clicked.connect(lambda: printNumList(window, numList))
+window.addButton(0, 0, 1, 10)
 
-#labelWidgets = addNumberListWidgets(window, 100)
+for n in range(100):
+	window.addEntry(random.randrange(0, 100), (n // 10) + 1, n % 10)
 
-#labelWidgets[1].clicked.connect(lambda: highlightSelectedNumber(labelWidgets))
+window.setGUI()
 
-#window.show()
+window.show()
 
-#app.exec()
+QTimer.singleShot(1000, lambda: swapLabels(0, 10))
+
+window.labelWidgets = mergeSortGUI(window.labelWidgets)
+
+
+
+print("merge finished")
+
+window.setGUI()
+
+window.show()
+
+app.exec()
 
